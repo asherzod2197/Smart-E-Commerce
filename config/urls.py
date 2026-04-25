00356bdd13971486294d7eabb4d57e9863cb9dfd
asherzod2197 <asherzod2197@gmail.com>
2026-04-25 22:40:51 +0500

    added

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('apps.users.urls')),
+    path('api/products/', include('apps.products.urls')),
+    # path('api/', include('apps.products.urls')),
 ]
