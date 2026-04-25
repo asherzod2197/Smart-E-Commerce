e507dab613c6be18d710421db97af6b2634eee8c
asherzod2197 <asherzod2197@gmail.com>
2026-04-25 22:40:18 +0500

    added

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,103 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
 
 # Create your views here.
+
+class CategoryListView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+
+        return Response(serializer.data)
+    
+
+class ProductListView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        products = Product.objects.filter(is_available=True)
+        serializer = ProductSerializer(products, many=True)
+
+        return Response(serializer.data)
+
+
+class ProductDetailView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, pk):        
+        try:
+            product = Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response(
+                {"error": "Mahsulot topilmadi!"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+
+class ProductCreateView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data, 
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, 
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class ProductUpdateView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def put(self, request, pk):
+        try:
+            product = Product.objects.get(pk=pk)
+
+            
+        except Product.DoesNotExist:
+            return Response(
+                {'error': "Mahsulot topilmadi!"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class ProductDeleteView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def delete(self, request, pk):
+        try:
+            product = Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response(
+                {'error': "Mahsulot topilmadi!"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
